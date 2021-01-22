c14eeb4c8c9e9f9ccb461b190dfa48d1a4bc7bcf
Matt <madas@hawk.iit.edu>
2021-01-22 17:34:59 -0600

    Initial Commit

---
+++ Shared/VolumeAndSurfaceAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeAndSurfaceAreaApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 1/22/21.
+//
+
+import SwiftUI
+
+@main
+struct VolumeAndSurfaceAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
